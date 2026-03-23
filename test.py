7ae9b4e815b79fd5eb656ddc9d40322d0a6c6ccf
Dan Fuhry <dan@fuhry.com>
2026-03-23 18:20:00 -0400

    Use typing.Protocol for AioConfig's http_session_cls
    `AioConfig` accepts any type which implements an interface compatible with `AIOHTTPSession` for the `http_session_cls` argument, but the static stubs permit only the concrete class.
    
    This specifically causes `aiobotocore.httpxsession.HttpxSession`, added in aiobotocore 2.23.0, to be prohibited.
    
    Add an `_HTTPSessionProtocol` protocol, using narrowed method signatures from `AIOHTTPSession`, and change `type[AIOHTTPSession]` to `type[_HTTPSessionProtocol]` in `AioConfig.__init__` constructor signature.
    
    Update `AIOHTTPSession` stub's `send` method with the correct modern type signature.
    
    Test output: (the one error is pre-existing)
    
    ```
    $ uv run mypy ./test.py
    test.py:8: error: Argument "env" to "AioIMDSFetcher" has incompatible type "int"; expected "Optional[str]"  [arg-type]
    Found 1 error in 1 file (checked 1 source file)
    ```
    
    Signed-Off-By: Dan Fuhry <dan@fuhry.com>

--- test.py
+++ test.py
@@ -1,5 +1,10 @@
+from aiobotocore.config import AioConfig
+from aiobotocore.httpsession import AIOHTTPSession
+from aiobotocore.httpxsession import HttpxSession
 from aiobotocore.utils import AioIMDSFetcher
 
 fetcher = AioIMDSFetcher(env="dev")
 fetcher = AioIMDSFetcher(env=None)
 fetcher = AioIMDSFetcher(env=123)
+config = AioConfig(http_session_cls=AIOHTTPSession)
+config = AioConfig(http_session_cls=HttpxSession)
